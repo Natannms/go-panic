3902181c5452b4a3b976306b9c5fba56b90c07b1
Natannms <agnusnat.nms@gmail.com>
2023-03-09 19:05:18 -0300

    exemplo simples gerando panic

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func divide(a, b int) int {
+	return a / b
+}
+
+func main() {
+
+	fmt.Println(divide(4, 0))
+	fmt.Println(divide(4, 2))
+	fmt.Println("CONTINUEI EXECUTANDO")
+}
